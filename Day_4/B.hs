f29e60480e1ad2b942750b72fb71c657e2eda7ca
than5466 <than5466@student.su.se>
2020-01-28 20:38:34 +0100

    Dag 4

---
+++ Day_4/B.hs
@@ -0,0 +1,39 @@
+import System.IO  
+import Control.Monad
+import Data.Char
+import Data.List.Split
+
+
+
+quicksort :: Ord a => [a] -> [a]
+quicksort []     = []
+quicksort (p:xs) = (quicksort lesser) ++ [p] ++ (quicksort greater)
+    where
+        lesser  = filter (< p) xs
+        greater = filter (>= p) xs
+
+if_valid :: [String] -> Int
+if_valid strings =
+    if (length strings) == 1
+        then 1
+        else if (strings !! ((length strings)-1)) == (strings !! ((length strings)-2))
+            then 0
+            else if_valid (take ((length strings)-1) strings)
+
+
+f :: [String] -> [String]
+f = map quicksort
+
+count :: [String] -> Int
+count [] = 0
+count strings = (if_valid (quicksort (f (splitOn " " (strings !! ((length strings) -1)))))) + (count (take ((length strings)-1) strings))
+    
+
+main = do  
+        handle <- openFile "data.txt" ReadMode
+        contents <- hGetContents handle
+        let ans = count (splitOn "\n" contents)
+        print ans
+        
+        hClose handle
+        
