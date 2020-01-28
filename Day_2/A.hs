441896b9e2a7b81d2ef74dba18135a3e6ccef315
than5466 <than5466@student.su.se>
2020-01-28 10:06:36 +0100

    Dag 2

---
+++ Day_2/A.hs
@@ -0,0 +1,43 @@
+import System.IO  
+import Control.Monad
+import Data.Char
+import Data.List.Split
+
+max_func :: [String] -> Integer -> Integer
+max_func s x = 
+    if length s == 0
+    then x
+    else max_func (take ((length s)-1) s) (max_value (read (s !!((length s) -1)) :: Integer) x)
+
+max_value :: Integer -> Integer -> Integer
+max_value x y = 
+    if x > y
+    then x
+    else y
+
+min_func :: [String] -> Integer -> Integer
+min_func s x = 
+    if length s == 0
+    then x
+    else min_func (take ((length s)-1) s) (min_value (read (s !!((length s)-1)) :: Integer) x)
+
+min_value :: Integer -> Integer -> Integer
+min_value x y = 
+    if x < y
+    then x
+    else y
+
+checksum :: [String] -> Integer
+checksum s = 
+    if length s == 0
+    then 0
+    else (max_func (splitOn "\t" (s !! (length s - 1))) 0 - min_func(splitOn "\t" (s !! (length s - 1))) 1000) + checksum (take ((length s)-1) s)
+
+main = do  
+        handle <- openFile "data.txt" ReadMode
+        contents <- hGetContents handle
+        let ans = checksum (splitOn "\n" contents)
+        print ans
+        
+        hClose handle
+        
---
+++ Day_2/B.hs
@@ -0,0 +1,33 @@
+import System.IO  
+import Control.Monad
+import Data.Char
+import Data.List.Split
+
+check_divisor :: [String] -> Int -> Int -> Int -> Int
+check_divisor s a b n =
+    if mod (read (s !! a)::Int) (read (s !! b)::Int) == 0
+        then div (read (s !! a)::Int)  (read (s !! b)::Int)
+        else if mod (read (s !! b)::Int) (read (s !! a)::Int) == 0
+            then div (read (s !! b)::Int)  (read (s !! a)::Int)
+            else if b == n 
+                then check_divisor s (a+1) (a+2) n
+                else check_divisor s a (b+1) n
+
+divisor :: [String] -> Int -> Int -> Int
+divisor s a b = check_divisor s a b ((length s) -1)
+
+
+checksum :: [String] -> Int
+checksum s = 
+    if length s == 0
+    then 0
+    else (divisor (splitOn "\t" (s !! (length s - 1))) 0 1) + checksum (take ((length s)-1) s)
+
+main = do  
+        handle <- openFile "data.txt" ReadMode
+        contents <- hGetContents handle
+        let ans = checksum (splitOn "\n" contents)
+        print ans
+        
+        hClose handle
+        
